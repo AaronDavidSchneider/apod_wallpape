8f19e5035b3760e15df0279126a0a348cdce07fa
Aaron David Schneider <aaron.david.schneider@gmail.com>
2020-03-02 08:27:48 +0100

    now: only execute if number of screens larger or equals start screen

--- apod_daily.py
+++ apod_daily.py
@@ -28,44 +28,45 @@ def purge(dir):  # only used to clean up old images
 def main(start_display, web):
     p = Popen(['osascript', '-'], stdin=PIPE, stdout=PIPE, stderr=PIPE, universal_newlines=True)
     screen_num, stderr = p.communicate('''tell application "System Events" to return count of desktops''')
-    response = requests.get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY").json()
-    media_type = response["media_type"]
-    hdurl = response["hdurl"]
-    url = response["url"]
-    apod_date = response["date"]
-    tmp_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), "tmp")
-    apod_file = os.path.join(tmp_dir, "apod_{}.jpg".format(apod_date))
+    if int(screen_num) >= start_display:
+        response = requests.get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY").json()
+        media_type = response["media_type"]
+        hdurl = response["hdurl"]
+        url = response["url"]
+        apod_date = response["date"]
+        tmp_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), "tmp")
+        apod_file = os.path.join(tmp_dir, "apod_{}.jpg".format(apod_date))
 
-    if not os.path.exists(tmp_dir):
-        os.makedirs(tmp_dir)
+        if not os.path.exists(tmp_dir):
+            os.makedirs(tmp_dir)
 
-    if web:
-        webbrowser.open("https://apod.nasa.gov/apod/astropix.html")
-    if media_type == "image":
-        purge(tmp_dir)  # clean up old images
-        r = requests.get(hdurl, stream=True)
-        if r.status_code == 200:
-            with open(apod_file, "wb") as f:
-                r.raw.decode_content = True
-                shutil.copyfileobj(r.raw, f)
+        if web:
+            webbrowser.open("https://apod.nasa.gov/apod/astropix.html")
+        if media_type == "image":
+            purge(tmp_dir)  # clean up old images
+            r = requests.get(hdurl, stream=True)
+            if r.status_code == 200:
+                with open(apod_file, "wb") as f:
+                    r.raw.decode_content = True
+                    shutil.copyfileobj(r.raw, f)
 
-        for i in range(start_display, max(int(screen_num) + 1,start_display+1)):
-            scpt = '''
-                tell application "System Events"
-                    tell desktop {}
-                        set picture rotation to 0
-                        set picture to "{}"
+            for i in range(start_display, int(screen_num)+1, start_display+1):
+                scpt = '''
+                    tell application "System Events"
+                        tell desktop {}
+                            set picture rotation to 0
+                            set picture to "{}"
+                        end tell
                     end tell
-                end tell
-                '''.format(i, apod_file)
+                    '''.format(i, apod_file)
 
-            p = Popen(['osascript', '-'], stdin=PIPE, stdout=PIPE, stderr=PIPE, universal_newlines=True)
-            stdout, stderr = p.communicate(scpt)
+                p = Popen(['osascript', '-'], stdin=PIPE, stdout=PIPE, stderr=PIPE, universal_newlines=True)
+                stdout, stderr = p.communicate(scpt)
 
-    elif media_type == "video":
-        print("Today is a video, please visit https://apod.nasa.gov/apod/astropix.html for more information.")
-    else:
-        print("Error! Today APOD is something weird.")
+        elif media_type == "video":
+            print("Today is a video, please visit https://apod.nasa.gov/apod/astropix.html for more information.")
+        else:
+            print("Error! Today APOD is something weird.")
 
 
 if __name__ == "__main__":
